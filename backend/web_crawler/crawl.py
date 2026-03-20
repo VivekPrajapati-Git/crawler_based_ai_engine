fbf0f9a57831ce50f947d88d800acacc9a5e451f
VivekPrajapati-Git <vivekbusiness20@gmail.com>
2026-03-20 12:51:09 +0530

    Added : Crawling the data through links

--- backend/web_crawler/crawl.py
+++ backend/web_crawler/crawl.py
@@ -1,8 +1,36 @@
+import requests
+import time
 from bs4 import BeautifulSoup
-from .link import link
+from link import get_top_link
+import re
 
 def crawl(data):
-    links = link(data)
+    links = get_top_link(data) #Getting the top links
+    print(links)
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept": "text/html,application/xhtml+xml",
+        "Connection": "keep-alive"
+    }
 
+    # Iterating over the link to parse the web page
     for url in links:
-        print(url)
+        # Fetching the documents using requests method
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            html_content = response.text # Getting the content
+            soup = BeautifulSoup(html_content,'lxml')
+            count = 1
+            result = re.findall("<p>")
+            print(result)
+            # with open(f'text{count}.html', 'w', encoding='utf-8') as f:
+            #     f.write(html_content)
+            # count = count+1
+            time.sleep(3)
+        else:
+            print(f"Failed to retrieve page: {response.status_code}")
+
+
+crawl("machinelearning")
--- backend/web_crawler/link.py
+++ backend/web_crawler/link.py
@@ -5,7 +5,8 @@ from serpapi import GoogleSearch
 load_dotenv()
 serpapi_key = os.getenv("serpapi_key")
 
-def link(data):
+def get_top_link(data):
+    #Defining the params for serpapi for fetching the result
     params = {
         'engine' : 'google',
         'q' : data,
@@ -13,9 +14,10 @@ def link(data):
     }
 
     search = GoogleSearch(params)
-    result = search.get_dict()
+    result = search.get_dict() #Getting the result
     
     links = []
+    # Appending all the links that is visited
     for result in result.get('organic_results',[]):
         links.append(result["link"])
 
